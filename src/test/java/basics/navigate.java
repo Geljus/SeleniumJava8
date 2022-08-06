d53d35135fea945705810624fc7563713a75dd5e
geljus <geljus@gmail.com>
2022-08-06 18:47:51 +0300

    Lesson_1

--- src/test/java/basics/navigate.java
+++ src/test/java/basics/navigate.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class navigate {
